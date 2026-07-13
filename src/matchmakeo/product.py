ecb59f8806d3c6b3ba6530028db5ac4407d976eb
davidwyld <24752124+davidwilby@users.noreply.github.com>
2026-07-13 15:10:35 +0100

    add/test product.get_table method

--- src/matchmakeo/product.py
+++ src/matchmakeo/product.py
@@ -1,10 +1,16 @@
 from dataclasses import dataclass, field
-from pathlib import Path
-from tempfile import TemporaryDirectory
+
+import sqlalchemy
+from sqlalchemy.exc import NoSuchTableError
+
+from .databases import Database
+from .utils import setUpLogging
 
 __all__ = [
     "Product"
 ]
+
+log = setUpLogging(__name__)
 
 @dataclass
 class Product:
@@ -16,3 +22,14 @@ class Product:
     extra_fields: list = field(default_factory=list)
     version: int = None
 
+    def get_table(self, database: Database):
+        "The table object corresponding to the product."
+
+        inspector = sqlalchemy.inspect(database.create_engine())
+        
+        # Check if the table exists in the database first
+        if not inspector.has_table(self.table_name):
+            raise ValueError(f"Table '{self.table_name}' does not exist in the database.")
+        
+        metadata = sqlalchemy.MetaData()
+        return sqlalchemy.Table(self.table_name, metadata, autoload_with=database.engine)
