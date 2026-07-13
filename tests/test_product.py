e2dd7b549534c4f0785b3afd096d8e71638dccb9
davidwyld <24752124+davidwilby@users.noreply.github.com>
2026-07-13 15:10:35 +0100

    add/test product.get_table method

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,4 +1,5 @@
 import pytest
+import sqlalchemy
 
 from matchmakeo.product import Product
 
@@ -14,3 +15,26 @@ def test_product():
 
     with pytest.raises(TypeError):
         p = Product(table_name="MOD021KM")
+
+def test_product_table(database):
+    prod_name = "test_prod_name"
+    product = Product(name=prod_name, table_name=prod_name)
+    
+    # test for error on no table
+    with pytest.raises(ValueError):
+        product.get_table(database)
+
+    # create the table
+    metadata = sqlalchemy.MetaData()
+    sqlalchemy.Table(
+        prod_name,
+        metadata,
+        sqlalchemy.Column("pk", sqlalchemy.Integer, primary_key=True)
+        ).create(database.connect(), checkfirst=True)
+    database.connection.commit()
+
+    # test table is correct
+    table = product.get_table(database)
+    assert isinstance(table, sqlalchemy.Table)
+    assert table.name == product.table_name
+    
